fbc74c4822a20221ce8a22d77a37e1365ccd1980
MKS-Student <mks.student@hackreactor.com>
2015-03-23 16:47:34 -0500

    mocking out model methods, and other setup

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,39 @@
-// SongQueue.js - Defines a backbone model class for the song queue.
+// SongQueue.js - Defines a backbone collection class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  enqueue: function(song){
+    //increment end
+    //add song at new end index
+  },
+
+  dequeue: function(song){
+    //increment start
+    //delete previous start
+  },
+
+  playFirst: function(){
+    // set SongQueue[0] to currentSong 
+  },
+
+  playNext: function(){
+
   }
 
 });
+
+/*
+
+for backbone, start with the views
+what are you listening for? 
+And what methods do you call when you hear those events?
+Add that to the model
+
+In SongQueue
+methods: initialize, enqueue, dequeue, playNextSong
+
+In SongQueueView
+listen for a click on the add to queue button -> call enqueue
+listen for the end of the track -> call playNextSong (and dequeue?)
+
+
+*/
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,21 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
   },
 
-  render: function() {
-    return this.$el;
+  render: function() { 
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      //for each song in the library, map a function that calls a library entry view
+      // this.collection.
+        return new SongQueueEntryView({model: song}).render()
+      })
+    );
   }
 
 });
